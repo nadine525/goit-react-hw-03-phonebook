b7c66ea499f97dce47a019eeff27d7f8d78e10da
nadine525 <02_seeders_wide@icloud.com>
2023-06-12 13:15:43 +0200

    methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,6 +43,22 @@ export class App extends Component {
     const normalazedFilter = filter.toLowerCase();
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalazedFilter));
   }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    // console.log(parsedContacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
   
   render() {
     const {  filter } = this.state;
